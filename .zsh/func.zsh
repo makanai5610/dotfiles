753e848a5d1e1c2b9edc18974f339dbdf50964d6
makanai5610 <takumi.k.5610@gmail.com>
2021-02-27 20:54:35 +0900

    update

--- .zsh/func.zsh
+++ .zsh/func.zsh
@@ -57,37 +57,98 @@ function ghq_update() {
 function ghq_master() {
     local current=$PWD
     for repo in $(ghq list --full-path); do
-        cd $repo
         echo_success 'pull '
         echo "$repo"
+
+        cd $repo
         git switch master
+
         echo
     done
+
     cd $current
 }
 
 function cdg() {
     local repo=$(ghq list --full-path | peco)
-    if [ -n "$repo" ]; then
-        cd $repo
+    if [ -z "$repo" ]; then
+        echo_failure "canceled\n"
+        reset_style
+        return
     fi
+
+    echo_success "selected "
+    reset_style
+    echo "$dir"
+
+    cd $repo
+}
+
+function cdp() {
+    local dir=$(/bin/ls -Fa -1 | grep / | peco)
+    if [ -z "$dir" ]; then
+        echo_failure "canceled\n"
+        reset_style
+        return
+    fi
+
+    echo_success "selected "
+    reset_style
+    echo "$dir"
+
+    while [ $(/bin/ls -F $dir | grep / | wc -l) != 0 ]; do
+        /bin/ls -Fa -1 $dir | grep / | peco | read s
+        if [ -z "$s" ]; then break; fi
+        dir="$dir$s"
+
+        echo_success "selected "
+        reset_style
+        echo "$dir"
+    done
+
+    cd $dir
 }
 
 function codeg() {
     local repo=$(ghq list --full-path | peco)
-    if [ -n "$repo" ]; then
-        code $repo
+    if [ -z "$repo" ]; then
+        echo_failure "canceled\n"
+        reset_style
+        return
     fi
+
+    echo_success "selected "
+    reset_style
+    echo "$repo"
+
+    code $repo
 }
 
 function batg() {
     local dir=$(ghq list --full-path | peco)
-    if [ -z "$dir" ]; then return; fi
+    if [ -z "$dir" ]; then
+        echo_failure "canceled\n"
+        reset_style
+        return
+    fi
+
+    echo_success "selected "
+    reset_style
+    echo "$dir"
 
     while [ -d $dir ]; do
         /bin/ls -a -1 $dir | peco | read s
-        if [ -z "$s" ]; then return; fi
+        if [ -z "$s" ]; then
+            echo_failure "canceled\n"
+            reset_style
+            return
+        fi
+
         dir="$dir/$s"
+
+        echo_success "selected "
+        reset_style
+        echo "$dir"
     done
 
     bat $dir
@@ -95,12 +156,29 @@ function batg() {
 
 function vimg() {
     local dir=$(ghq list --full-path | peco)
-    if [ -z "$dir" ]; then return; fi
+    if [ -z "$dir" ]; then
+        echo_failure "canceled\n"
+        reset_style
+        return
+    fi
+
+    echo_success "selected "
+    reset_style
+    echo "$dir"
 
     while [ -d $dir ]; do
         /bin/ls -a -1 $dir | peco | read s
-        if [ -z "$s" ]; then return; fi
+        if [ -z "$s" ]; then
+            echo_failure "canceled\n"
+            reset_style
+            return
+        fi
+
         dir="$dir/$s"
+
+        echo_success "selected "
+        reset_style
+        echo "$dir"
     done
 
     vim $dir
