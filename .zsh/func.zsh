de5c3c1e2a4d6c820708f0ca1d01e34d53423111
makanai5610 <takumi.k.5610@gmail.com>
2021-03-05 21:55:12 +0900

    update

--- .zsh/func.zsh
+++ .zsh/func.zsh
@@ -22,7 +22,7 @@ function kc_prd() {
 
 function ksh() {
     if [ $# -eq 1 ]; then
-        ke -it -n $1 $(kg pod -n $1 | sed -n 2p | awk '{print $1}') -- sh
+        kex -it -n $1 $(kg pod -n $1 | sed -n 2p | awk '{print $1}') -- sh
     else
         echo_failure 'must input namespace.\n'
         kg namespaces
@@ -31,7 +31,7 @@ function ksh() {
 
 function krc() {
     if [ $# -eq 1 ]; then
-        ke -it -n $1 $(kg pod -n $1 | sed -n 2p | awk '{print $1}') -- bin/rails c
+        kex -it -n $1 $(kg pod -n $1 | sed -n 2p | awk '{print $1}') -- bin/rails c
     else
         echo_failure 'must input namespace.\n'
         kg namespaces
--- link.zsh
+++ link.zsh
@@ -1,25 +1,38 @@
 #!/bin/zsh
 
-filepathes=(
-    "/git"
-    "/vim"
-    "/p10k"
-    ""
-)
-
-filenames=(
+local file_names=(
     ".gitconfig"
     ".vimrc"
     ".p10k.zsh"
     ".zshrc"
+    "move_word.json"
 )
 
-if test $#filepathes -ne $#filenames; then
-    echo "filepathes count is not equal filenames count"
-    echo "filepathes=$#filepathes, filenames=$#filenames"
+local from_file_pathes=(
+    "/git"
+    "/vim"
+    "/p10k"
+    ""
+    "/karabiner"
+)
+
+local to_file_pathes=(
+    "$HOME"
+    "$HOME"
+    "$HOME"
+    "$HOME"
+    "$HOME/.config/karabiner/assets/complex_modifications"
+)
+
+if [ $#file_names -ne $#from_file_pathes -o $#from_file_pathes -ne $#to_file_pathes ]; then
+    echo "file_names, from_file_pathes and to_file_pathes count is not equal each other."
+    echo "file_names=$#file_names, from_file_pathes=$#from_file_pathes, to_file_pathes=$#to_file_pathes"
+    exit 1
 fi
 
-for i in $(seq 1 $#filepathes); do
-    echo "$HOME/${filenames[$i]} -> $PWD${filepathes[$i]}/${filenames[$i]}"
-    ln -si "$PWD${filepathes[$i]}/${filenames[$i]}" "$HOME"
+local work_dir=$(git rev-parse --show-toplevel)
+
+for i in $(seq 1 $#from_file_pathes); do
+    echo "${to_file_pathes[$i]}/${file_names[$i]} -> $work_dir${from_file_pathes[$i]}/${file_names[$i]}"
+    ln -si "$work_dir${from_file_pathes[$i]}/${file_names[$i]}" "${to_file_pathes[$i]}"
 done
