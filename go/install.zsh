3e31a999e6ada77b886355f7b4345152b696d8aa
karrybit <takumi.k.5610@gmail.com>
2021-05-03 14:57:33 +0900

    update

---
+++ go/install.zsh
@@ -0,0 +1,16 @@
+#!/usr/local/bin/zsh
+
+function install() {
+    if [ "$1" = 'dry' ]; then
+        while read package; do
+            echo "go install $package@latest"
+        done <$DOTFILES_PATH/go/package.txt
+    else
+        while read package; do
+            go install "$package@latest"
+        done <$DOTFILES_PATH/go/package.txt
+    fi
+}
+
+# if $1 is 'dry', dry run.
+install $1
